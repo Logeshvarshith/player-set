f3316e36fc06d25e623fffc97ac5d22a5de4df11
Logeshvarshith <36299174+Logeshvarshith@users.noreply.github.com>
2018-05-25 10:06:27 +0530

    Create 42.py

---
+++ 42.py
@@ -0,0 +1,12 @@
+n=int(input("enter a numbers"))
+l=[]
+m=[]
+for i in range(0,n):
+    b=input("enter string")
+    l.append(b)
+    m.append(b)
+l.sort()
+if(m==l):
+  print("yes")
+else:
+  print("no")
